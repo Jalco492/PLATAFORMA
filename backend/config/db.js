5ca2093dc06af8d26b2ffb4f5bc7ffb99ef448e0
Jalco492 <johanlopezcordoba9@gmail.com>
2026-07-28 11:31:31 -0600

    DB EN LINEA

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2/promise");
 
 const connection = mysql.createPool({
-  host: "sql5.freesqldatabase.com",
-  user: "sql5834019",
-  password: "PyIDMXgTIf", // Déjalo vacío si no tiene contraseña
-  database: "sql5834019", // Cambia por el nombre de tu base de datos
+  host: "sakura.proxy.rlwy.net",
+  user: "root",
+  password: "QHEQhXsonjshFWxDWDHlTXyJxvdRXxFq", // Déjalo vacío si no tiene contraseña
+  database: "railway", // Cambia por el nombre de tu base de datos
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
